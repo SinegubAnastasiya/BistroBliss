56f12bdc65e4d62d7490dd0ac27b7ea999536e84
SinegubAnastasiya <fedukevich98@mail.ru>
2024-08-03 20:14:17 +0300

    added fonts and fixed styles for Header

--- bistro/src/components/Header/Header.js
+++ bistro/src/components/Header/Header.js
@@ -4,8 +4,8 @@ import style from './header.module.css'
 export default function Header() {
     return <header>
         <div className={style.grey_container}>
-            <div className={style.wrapper}>
-                <div className={style.info}>
+            <div className={style.info}>
+                <div className={style.infoContacts}>
                     <div className={style.phone}>
                         <div className={style.imgPhone}></div>
                         <p>(414) 857 - 0107</p>
@@ -14,19 +14,19 @@ export default function Header() {
                         <div className={style.imgMail}></div>
                         <a href=''>yummy@bistrobliss</a>
                     </div>
-                    <div className={style.icons}>
-                        <div className={style.iconBlock}>
-                            <div className={style.twitter}></div>
-                        </div>
-                        <div className={style.iconBlock}>
-                            <div className={style.facebook}></div>
-                        </div>
-                        <div className={style.iconBlock}>
-                            <div className={style.instagram}></div>
-                        </div>
-                        <div className={style.iconBlock}>
-                            <div className={style.gitHub}></div>
-                        </div>
+                </div>
+                <div className={style.icons}>
+                    <div className={style.iconBlock}>
+                        <div className={style.twitter}></div>
+                    </div>
+                    <div className={style.iconBlock}>
+                        <div className={style.facebook}></div>
+                    </div>
+                    <div className={style.iconBlock}>
+                        <div className={style.instagram}></div>
+                    </div>
+                    <div className={style.iconBlock}>
+                        <div className={style.gitHub}></div>
                     </div>
                 </div>
             </div>
